b2b0163230a8ffca67a9af17f67cd16ce839bb4d
Krzysztof Oniszczuk <krzysiex01@gmail.com>
2019-10-21 18:42:19 +0200

    JobOffer class added, job offers view added, navBar button added, JobsOfferController added.

---
+++ HRWebApplication/Controllers/JobOfferController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HRWebApplication.Models;
+
+namespace HRWebApplication.Controllers
+{
+    [Route("[controller]")]
+    public class JobOfferController : Controller
+    {
+        private static List<JobOfferViewModel> jobOfferViewModels = new List<JobOfferViewModel>
+            {
+                new Models.JobOfferViewModel() { Id = 1, Title= "Plumber", Description="Looking for experienced plumber."},
+                new Models.JobOfferViewModel() { Id = 2, Title= "Advanced Plumber",Description = "Looking for Mario."},
+                new Models.JobOfferViewModel() { Id = 3, Title= "Senior Plumber",Description = "Looking for senior plumber."},
+            };
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View(jobOfferViewModels);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var offer = jobOfferViewModels.FirstOrDefault(o => o.Id == id);
+            return View(offer);
+        }
+    }
+}
---
+++ HRWebApplication/Models/JobOfferViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRWebApplication.Models
+{
+    public class JobOfferViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Salary { get; set; }
+        public List<string> Requirements { get; set; }
+    }
+}
