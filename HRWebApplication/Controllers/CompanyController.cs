5ba144828f23530abcbddf0369f41d2023f182b1
Krzysztof Oniszczuk <krzysiex01@gmail.com>
2019-10-28 18:15:05 +0100

    Creating and editing coampny data.

--- HRWebApplication/Controllers/CompanyController.cs
+++ HRWebApplication/Controllers/CompanyController.cs
@@ -18,6 +18,70 @@ namespace HRWebApplication.Controllers
             _context = context;
         }
 
+        public IActionResult Create()
+        {
+            return View(new Company());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Company model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _context.Companies.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id should not be null");
+            }
+
+            _context.Companies.Remove(new Company() { Id = id.Value });
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest($"id shouldn't not be null");
+            }
+            var offer = await _context.Companies.FirstOrDefaultAsync(x => x.Id == id.Value);
+
+            if (offer == null)
+            {
+                return NotFound($"company not found in DB");
+            }
+
+            return View(offer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Company model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var company =  await _context.Companies.FirstOrDefaultAsync(x => x.Id == model.Id);
+            company.Name = model.Name;
+            //TODO: Enable changing other fileds
+            _context.Update(company);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", null);
+        }
+
         public async Task<IActionResult> Index()
         {
             return View(await _context.Companies.ToListAsync());
