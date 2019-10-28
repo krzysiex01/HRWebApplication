f3e969664a19da428aa27cf0e1e2591bd27cc03d
Krzysztof Oniszczuk <krzysiex01@gmail.com>
2019-10-28 01:07:18 +0100

    Updated Create new offer view, new prperties added, basic edit view created.

--- HRWebApplication/Models/JobOffer.cs
+++ HRWebApplication/Models/JobOffer.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using HRWebApplication.Models;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace HRWebApplication.Models
 {
     public class JobOffer
     {
         public int Id { get; set; }
+        [Display(Name = "Job title")]
+        [Required]
         public string Title { get; set; }
         public virtual Company Company { get; set; }
         public virtual int CompanyId { get; set; }
+        [Required]
+        [MinLength(5)]
         public string Description { get; set; }
+        [Display(Name = "Overview")]
         public string Overview { get; set; }
+        [Display(Name = "Salary from")]
         public int SalaryFrom { get; set; }
+        [Display(Name = "Salary to")]
         public int SalaryTo { get; set; }
+        [Display(Name = "Currency")]
         public Currency Currency { get; set; }
+        [Display(Name = "Location")]
         public string Location { get; set; }
         public DateTime AddedOn { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
+        [Display(Name = "Valid until")]
         public DateTime? ValidUntil { get; set; }
+        [Display(Name = "Specialization")]
         public string Specialization { get; set; }
 
         public List<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
