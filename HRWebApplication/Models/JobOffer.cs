158a2efa65910c865f05c53e82b9d5569337b4ab
Krzysztof Oniszczuk <krzysiex01@gmail.com>
2019-11-11 02:36:45 +0100

    Client-side validation using JQuery.

--- HRWebApplication/Models/JobOffer.cs
+++ HRWebApplication/Models/JobOffer.cs
@@ -12,24 +12,29 @@ namespace HRWebApplication.Models
     public class JobOffer
     {
         public int Id { get; set; }
-        [Display(Name = "Job title")]
+
         [Required(ErrorMessage = "Your offer has no title!")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Job title")]
         public string Title { get; set; }
         public virtual Company Company { get; set; }
         public virtual int CompanyId { get; set; }
+
         [Required(ErrorMessage = "Please enter description of your offer.")]
         [MinLength(10,ErrorMessage = "Your description is too short.")]
         public string Description { get; set; }
-        [MaxLength(100,ErrorMessage = "Your overview is too long.")]
+
         [Required(ErrorMessage = "Please enter overview of your offer.")]
+        [MaxLength(100,ErrorMessage = "Your overview is too long.")]
         [Display(Name = "Overview")]
         public string Overview { get; set; }
-        [Display(Name = "Salary from")]
         [Range(1, int.MaxValue, ErrorMessage = "Salary has to be greater than 0.")]
+        [Display(Name = "Salary from")]
         public int SalaryFrom { get; set; }
-        [Display(Name = "Salary to")]
+
         [Range(1, int.MaxValue, ErrorMessage = "Salary has to be greater than 0.")]
         [IsGreaterThan("SalaryFrom",true, ErrorMessage = "Incorrect value.")]
+        [Display(Name = "Salary to")]
         public int SalaryTo { get; set; }
         [Display(Name = "Currency")]
         public Currency Currency { get; set; }
@@ -38,9 +43,10 @@ namespace HRWebApplication.Models
         public DateTime AddedOn { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyy-MM-dd}")]
-        [Display(Name = "Valid until")]
         [IsPastDate(true, ErrorMessage = "Selected date is invalid.")]
+        [Display(Name = "Valid until")]
         public DateTime? ValidUntil { get; set; }
+
         [Required(ErrorMessage = "Please enter specialization.")]
         [Display(Name = "Specialization")]
         public string Specialization { get; set; }
--- HRWebApplication/Startup.cs
+++ HRWebApplication/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace HRWebApplication
 {
@@ -26,6 +27,11 @@ namespace HRWebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+           
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
 
             var connection = Configuration["DatabaseConnectionString"];
             services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
@@ -35,6 +41,16 @@ namespace HRWebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
